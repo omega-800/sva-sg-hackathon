bb627a15f884953c6edb0d40fa6a3b3e5d48efad
omega-800 <georgiy.shevoroshkin@ost.ch>
2026-02-13 22:03:23 +0100

    adding util fns

---
+++ frontend/src/util/util.ts
@@ -0,0 +1,37 @@
+import type { Prim, Operation, UserData } from "../types";
+
+export const isPrim = (elem: any): elem is Prim =>
+  ["number", "string", "boolean"].includes(typeof elem);
+
+export const isObjPath = (elem: any): elem is Array<string> =>
+  Array.isArray(elem) && elem.every((i) => typeof i == "string");
+
+export const getAtObjPath = (elem: object, path: Array<string>) =>
+  path.reduce((acc, k) => acc?.[k], elem);
+
+export const evalFlowOperation = (
+  ctx: UserData,
+  o: Operation | Prim | Array<string>,
+) => {
+  if (isPrim(o)) return o;
+  if (isObjPath(o)) return getAtObjPath(ctx, o);
+  let evalOp = {
+    op: o.op,
+    ...("val" in o ? { val: evalFlowOperation(ctx, o.val) } : {}),
+    lhs: evalFlowOperation(ctx, o.lhs),
+    rhs: evalFlowOperation(ctx, o.rhs),
+  };
+  if (evalOp.op == "if") {
+    return evalOp.val ? evalOp.lhs : evalOp.rhs;
+  } else if (evalOp.op == "sub") {
+    return evalOp.lhs - evalOp.rhs;
+  } else if (evalOp.op == "add") {
+    return evalOp.lhs + evalOp.rhs;
+  } else if (evalOp.op == "lt") {
+    return evalOp.lhs < evalOp.rhs;
+  } else if (evalOp.op == "gt") {
+    return evalOp.lhs > evalOp.rhs;
+  } else if (evalOp.op == "eq") {
+    return evalOp.lhs == evalOp.rhs;
+  }
+};
