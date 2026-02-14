4e916e5844049dc58a2528bf9ae5b7c89f65bdab
Edoardo Balsamo <edoardo.balsamo@ost.ch>
2026-02-14 11:20:17 +0100

    endings

---
+++ frontend/src/components/BadEnding.vue
@@ -0,0 +1,12 @@
+<template>
+    <v-sheet class="pa-4 text-center mx-auto" elevation="12" max-width="600" rounded="lg" width="100%">
+        <v-icon class="mb-5" color="red" icon="mdi-cancel" size="112"></v-icon>
+        <h2 class="text-h5 mb-6">Nope</h2>
+        <v-divider class="mb-4"></v-divider>
+        <div class="text-end">
+            <v-btn class="text-none" color="red" variant="flat" width="90" rounded>
+                Done
+            </v-btn>
+        </div>
+    </v-sheet>
+</template>
